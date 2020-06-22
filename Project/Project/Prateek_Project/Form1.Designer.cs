89be781bd03f68011bd8c558d2abd4a05b4f0657
phrinix <60997906+phrinix@users.noreply.github.com>
2020-06-22 04:14:21 -0400

    Add files via upload

---
+++ Project/Project/Prateek_Project/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Prateek_Project
+{
+    partial class DatabaseAppF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMS = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMSI = new System.Windows.Forms.ToolStripMenuItem();
+            this.databaseMS = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectMSI = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectMSI = new System.Windows.Forms.ToolStripMenuItem();
+            this.commandMS = new System.Windows.Forms.ToolStripMenuItem();
+            this.runqueryMSI = new System.Windows.Forms.ToolStripMenuItem();
+            this.readfileMSI = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputRTB = new System.Windows.Forms.RichTextBox();
+            this.resultDG = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resultDG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMS,
+            this.databaseMS,
+            this.commandMS});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(600, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileMS
+            // 
+            this.fileMS.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitMSI});
+            this.fileMS.Name = "fileMS";
+            this.fileMS.Size = new System.Drawing.Size(37, 20);
+            this.fileMS.Text = "File";
+            // 
+            // exitMSI
+            // 
+            this.exitMSI.Name = "exitMSI";
+            this.exitMSI.Size = new System.Drawing.Size(92, 22);
+            this.exitMSI.Text = "Exit";
+            this.exitMSI.Click += new System.EventHandler(this.exitMSI_Click);
+            // 
+            // databaseMS
+            // 
+            this.databaseMS.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectMSI,
+            this.disconnectMSI});
+            this.databaseMS.Name = "databaseMS";
+            this.databaseMS.Size = new System.Drawing.Size(67, 20);
+            this.databaseMS.Text = "Database";
+            // 
+            // connectMSI
+            // 
+            this.connectMSI.Name = "connectMSI";
+            this.connectMSI.Size = new System.Drawing.Size(133, 22);
+            this.connectMSI.Text = "Connect";
+            this.connectMSI.Click += new System.EventHandler(this.connectMSI_Click);
+            // 
+            // disconnectMSI
+            // 
+            this.disconnectMSI.Enabled = false;
+            this.disconnectMSI.Name = "disconnectMSI";
+            this.disconnectMSI.Size = new System.Drawing.Size(133, 22);
+            this.disconnectMSI.Text = "Disconnect";
+            this.disconnectMSI.Click += new System.EventHandler(this.disconnectMSI_Click);
+            // 
+            // commandMS
+            // 
+            this.commandMS.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runqueryMSI,
+            this.readfileMSI});
+            this.commandMS.Name = "commandMS";
+            this.commandMS.Size = new System.Drawing.Size(76, 20);
+            this.commandMS.Text = "Command";
+            // 
+            // runqueryMSI
+            // 
+            this.runqueryMSI.Name = "runqueryMSI";
+            this.runqueryMSI.Size = new System.Drawing.Size(180, 22);
+            this.runqueryMSI.Text = "Run Query";
+            this.runqueryMSI.Click += new System.EventHandler(this.runqueryMSI_Click);
+            // 
+            // readfileMSI
+            // 
+            this.readfileMSI.Name = "readfileMSI";
+            this.readfileMSI.Size = new System.Drawing.Size(180, 22);
+            this.readfileMSI.Text = "Read File";
+            this.readfileMSI.Click += new System.EventHandler(this.readfileMSI_Click);
+            // 
+            // inputRTB
+            // 
+            this.inputRTB.Location = new System.Drawing.Point(0, 25);
+            this.inputRTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inputRTB.Name = "inputRTB";
+            this.inputRTB.Size = new System.Drawing.Size(601, 134);
+            this.inputRTB.TabIndex = 1;
+            this.inputRTB.Text = "";
+            // 
+            // resultDG
+            // 
+            this.resultDG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultDG.Location = new System.Drawing.Point(0, 163);
+            this.resultDG.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.resultDG.Name = "resultDG";
+            this.resultDG.RowTemplate.Height = 24;
+            this.resultDG.Size = new System.Drawing.Size(600, 203);
+            this.resultDG.TabIndex = 2;
+            // 
+            // DatabaseAppF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.resultDG);
+            this.Controls.Add(this.inputRTB);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "DatabaseAppF";
+            this.Text = "DatabaseApp";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resultDG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMS;
+        private System.Windows.Forms.ToolStripMenuItem exitMSI;
+        private System.Windows.Forms.ToolStripMenuItem databaseMS;
+        private System.Windows.Forms.ToolStripMenuItem connectMSI;
+        private System.Windows.Forms.ToolStripMenuItem disconnectMSI;
+        private System.Windows.Forms.ToolStripMenuItem commandMS;
+        private System.Windows.Forms.ToolStripMenuItem runqueryMSI;
+        private System.Windows.Forms.ToolStripMenuItem readfileMSI;
+        private System.Windows.Forms.RichTextBox inputRTB;
+        private System.Windows.Forms.DataGridView resultDG;
+    }
+}
+
---
+++ Project/Project/Prateek_Project/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace Prateek_Project
+{
+    public partial class DatabaseAppF : Form
+    {
+
+        static string constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=database.accdb;Persist Security Info=False";
+        OleDbConnection conn = new OleDbConnection(constr);
+        OleDbCommand cmd = new OleDbCommand();
+
+        public DatabaseAppF()
+        {
+            InitializeComponent();
+            runqueryMSI.ShortcutKeys = Keys.F5;
+            runqueryMSI.ShowShortcutKeys = true;
+            readfileMSI.ShortcutKeys = Keys.F9;
+            readfileMSI.ShowShortcutKeys = true;
+        }
+
+        private void exitMSI_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void connectMSI_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                cmd.Connection = conn;
+                connectMSI.Enabled = false;
+                disconnectMSI.Enabled = true;
+            }
+            catch 
+            {
+                MessageBox.Show("Connection Failed!!");
+            }
+        }
+
+        private void disconnectMSI_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Close();
+                disconnectMSI.Enabled = false;
+                connectMSI.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Disconnection Failed!!");
+            }
+        }
+
+        private void runqueryMSI_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (disconnectMSI.Enabled == true)
+                {
+                    DataTable dataT = new DataTable();
+                    cmd.CommandText = inputRTB.Text;
+                    OleDbDataAdapter data = new OleDbDataAdapter(cmd);
+                    data.Fill(dataT);
+                    data.Dispose();
+                    resultDG.DataSource = dataT;
+                    StreamWriter sqlFile = File.AppendText("sqlFile.txt");
+                    DateTime now = DateTime.Now;
+                    sqlFile.Write(now.ToString("F") + "      " + inputRTB.Text + "\n");
+                    sqlFile.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Connect to database");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void readfileMSI_Click(object sender, EventArgs e)
+        {
+            ReadFileF readform = new ReadFileF();
+            readform.ShowDialog();
+        }
+    }
+}
